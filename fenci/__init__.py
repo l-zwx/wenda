2f57ba8d801c309ad4305a4cadd9331fa2b16db8
jianrong <2471788627@qq.com>
2017-10-20 20:29:51 +0800

    默认的

--- fenci/__init__.py
+++ fenci/__init__.py
@@ -1,7 +1,10 @@
 import jieba
-seg_list = jieba.cut("香农在信息论中提出的信息熵定义为自信息的期望",cut_all=True)
-print(" ".join(seg_list))
-seg_list = jieba.cut("香农在信息论中提出的信息熵定义为自信息的期望")
-print(" ".join(seg_list))
-seg_list = jieba.cut_for_search("香农在信息论中提出的信息熵定义为自信息的期望")
-print(" ".join(seg_list))
+from utils.read_data import *
+read_train_data()
+
+# seg_list = jieba.cut("香农在信息论中提出的信息熵定义为自信息的期望",cut_all=True)
+# print(" ".join(seg_list))
+# seg_list = jieba.cut("香农在信息论中提出的信息熵定义为自信息的期望")
+# print(" ".join(seg_list))
+# seg_list = jieba.cut_for_search("香农在信息论中提出的信息熵定义为自信息的期望")
+# print(" ".join(seg_list))
